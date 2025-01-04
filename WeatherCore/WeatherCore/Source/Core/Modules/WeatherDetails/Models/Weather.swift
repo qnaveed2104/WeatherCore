3f3ccdf77d5d7a25ec3ac8c91563ca8dcc4741dc
qazi <qazi.naveed@nextgeni.com>
2025-01-04 19:03:26 +0100

    adding model file for current weather api

---
+++ WeatherCore/WeatherCore/Source/Core/Modules/WeatherDetails/Models/Weather.swift
@@ -0,0 +1,12 @@
+//
+//  Weather.swift
+//  WeatherCore
+//
+//  Created by Qazi on 04/01/2025.
+//
+
+struct Weather: Codable {
+    let code: Int
+    let description: String
+    let icon: String
+}
