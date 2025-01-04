e49a43be64ca2c91a5d5f5c69e054e93116538f2
qazi <qazi.naveed@nextgeni.com>
2025-01-04 11:26:22 +0100

    Setup WeatherCore project with gitignore and LCENSE from gitHub

---
+++ WeatherCore/WeatherCore/WeatherCoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherCoreApp.swift
+//  WeatherCore
+//
+//  Created by Qazi on 04/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherCoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
