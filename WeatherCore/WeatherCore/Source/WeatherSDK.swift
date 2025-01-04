769ea8cf0408c43a7c192a66defb8d27da582314
qazi <qazi.naveed@nextgeni.com>
2025-01-04 13:39:46 +0100

    Adding required headers and sdk file that act as source of SDK

--- WeatherCore/WeatherCore/Source/WeatherSDK.swift
+++ WeatherCore/WeatherCore/Source/WeatherSDK.swift
@@ -8,18 +8,18 @@
 import Foundation
 import SwiftUI
 
+/// A class responsible for interacting with the Weather SDK to fetch weather information.
+///
+/// This class manages the API key and provides a method to fetch weather data for a specific city.
+/// It uses the provided `apiKey` to authenticate API requests.
 public final class WeatherSDK {
     
     private let apiKey: String
-    
     init (apiKey: String) {
         self.apiKey = apiKey
     }
     
-    
     func getWeather(city: String) -> AnyView? {
         return nil
     }
-    
-    
 }
