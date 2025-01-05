ed19615c256434f420273c3b0e2dbc2e87dc6b59
qazi <qazi.naveed@nextgeni.com>
2025-01-05 11:40:14 +0100

    Setting up WeatherServiceTests

---
+++ WeatherCore/WeatherCoreTests/Services/WeatherServiceTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherServiceTests.swift
+//  WeatherCoreTests
+//
+//  Created by Qazi on 05/01/2025.
+//
+
+import XCTest
+
+final class WeatherServiceTests: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+}
