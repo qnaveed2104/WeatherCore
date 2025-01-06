d095b165323fe439bcd2d00871caf40bf1e5f2cb
qazi <qazi.naveed@nextgeni.com>
2025-01-06 15:56:07 +0100

    handling weather data in view model

--- WeatherCore/WeatherCore/Source/Core/Modules/WeatherDetails/ViewModel/WeatherViewModel.swift
+++ WeatherCore/WeatherCore/Source/Core/Modules/WeatherDetails/ViewModel/WeatherViewModel.swift
@@ -9,26 +9,45 @@ import Foundation
 
 protocol WeatherViewModelProtocol: ObservableObject {
     var service: WeatherServiceProtocol { get }
+    var currentWeather: WeatherDisplayData? { get set}
+    var weatherForecast: [WeatherDisplayData] { get set}
+
     func fetchWeatherData() async
     func dismissSDK()
 }
 
 class WeatherViewModel: WeatherViewModelProtocol {
+    var currentWeather: WeatherDisplayData?
+    var weatherForecast: [WeatherDisplayData] = []
+    
     let service: WeatherServiceProtocol
     
     init(service: WeatherServiceProtocol) {
         self.service = service
     }
-        
+    
     func fetchWeatherData() async {
         do {
-            async let currentWeather = service.loadCurrentWeather()
-            async let weatherForecast = service.loadWeatherForecast()
-            let (_, _) = try await (currentWeather, weatherForecast)
+            async let currentWeatherTask: WeatherDisplayData? = loadCurrentWeather()
+            async let weatherForecastTask: [WeatherDisplayData] =  loadWeatherForecast()
+            
+            if let weather = try await currentWeatherTask {
+                self.currentWeather = weather
+            }
+            
+            self.weatherForecast = try await weatherForecastTask
             
         } catch {
             print(error)
         }
+    }
+    
+    private func loadCurrentWeather() async throws -> WeatherDisplayData? {
+        return try await service.loadCurrentWeather()
+    }
+    
+    private func loadWeatherForecast() async throws -> [WeatherDisplayData] {
+        return try await service.loadWeatherForecast()
     }
     
     func dismissSDK() {
