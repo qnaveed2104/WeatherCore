cd28a9ca25a58047eb0eb5c098e1e9f7af271ccf
qazi <qazi.naveed@nextgeni.com>
2025-01-06 12:33:58 +0100

    Implement Equatable protocol for Test cases

--- WeatherCore/WeatherCore/Source/Core/Networking/Error/AppError.swift
+++ WeatherCore/WeatherCore/Source/Core/Networking/Error/AppError.swift
@@ -5,7 +5,31 @@
 //  Created by Qazi on 04/01/2025.
 //
 
-public enum AppError: Error {
+public enum AppError: Error, Equatable {
+    public static func == (lhs: AppError, rhs: AppError) -> Bool {
+            switch (lhs, rhs) {
+            case (.invalidResponse, .invalidResponse),
+                 (.unknownError, .unknownError),
+                 (.emptyData, .emptyData),
+                 (.githubTokenNotfound, .githubTokenNotfound),
+                 (.noWeatherDataAvailable, .noWeatherDataAvailable),
+                 (.invalidWeatherData, .invalidWeatherData),
+                 (.invalidAPIKey, .invalidAPIKey),
+                 (.invalidCityName, .invalidCityName):
+                return true
+            case (.decodingError(let lhsError), .decodingError(let rhsError)):
+                return lhsError.localizedDescription == rhsError.localizedDescription
+            case (.invalidBaseUrl(let lhsDescription), .invalidBaseUrl(let rhsDescription)),
+                 (.invalidUrl(let lhsDescription), .invalidUrl(let rhsDescription)):
+                return lhsDescription == rhsDescription
+            case (.httpError(let lhsStatusCode), .httpError(let rhsStatusCode)):
+                return lhsStatusCode == rhsStatusCode
+            case (.missingCriticalField(let lhsField), .missingCriticalField(let rhsField)):
+                return lhsField == rhsField
+            default:
+                return false
+            }
+        }
     case invalidResponse
     case unknownError
     case decodingError(error: Error)
