95a9cc0b7ef31ae862d5028b1d5ad26d53b66d7c
qazi <qazi.naveed@nextgeni.com>
2025-01-06 13:14:29 +0100

    update mockData

--- WeatherCore/WeatherCoreTests/Services/WeatherServiceTests.swift
+++ WeatherCore/WeatherCoreTests/Services/WeatherServiceTests.swift
@@ -27,7 +27,7 @@ final class WeatherServiceTests {
     
     @Test
     func testLoadCurrentWeather() async {
-        repository.currentWeatherResponse = MockWeatherData.getData()
+        repository.currentWeatherResponse = MockWeatherData.getDataForCurrentWeather()
         service = WeatherService(weatherSDKDelegate: delegate, repository: repository)
         
         let result = try? await service?.loadCurrentWeather()
@@ -35,17 +35,28 @@ final class WeatherServiceTests {
         #expect(result?.cityName == "The Weather in Berlin is:")
         #expect(result?.fomattedTime == "AT 18:00 LOCAL TIME")
         #expect(result?.formatedTemp == "23°C")
-        #expect(result?.skyCondition == "Clouds")
+        #expect(result?.skyCondition == "Cloud")
     }
     
     @Test
     func testLoadCurrentWeather_InvalidResponse() async {
-        service = WeatherService(weatherSDKDelegate: delegate, repository: repository)        
+        service = WeatherService(weatherSDKDelegate: delegate, repository: repository)
         await #expect(performing: {
             try await service?.loadCurrentWeather()
         }, throws: { error in
             error as? AppError == .invalidResponse
         })
+    }
+    
+    @Test
+    func testLoadWeatherForecast() async {
+        repository.forecastResponse = MockWeatherData.getDataForWeatherForecast()
+        service = WeatherService(weatherSDKDelegate: delegate, repository: repository)
+
+        let result = try? await service?.loadWeatherForecast()
+        #expect(result?.count == 2)
+        #expect(result?[0].skyCondition == "Broken clouds")
+        #expect(result?[1].skyCondition == "Overcast clouds")
     }
     
     deinit {
