88940778c705722ae0e09b6c51a4bed7c2f31012
qazi <qazi.naveed@nextgeni.com>
2025-01-06 11:38:24 +0100

    First Test Case 1- testLoadCurrentWeather added

--- WeatherCore/WeatherCoreTests/Services/WeatherServiceTests.swift
+++ WeatherCore/WeatherCoreTests/Services/WeatherServiceTests.swift
@@ -17,13 +17,25 @@ final class WeatherServiceTests {
     init() async throws {
         
         let mockDecorder = MockDecoder()
-        let mockAPI = MockAPIClient(decoder: MockDecoder())
+        let mockAPI = MockAPIClient(decoder: mockDecorder)
         let mockRequestBuilder = MockRequestBuilder(
             configurations: Configurations(apiKey: "apikey", cityName: "Berlin", hours: 24)
         )
         repository = MockWeatherRepository(apiClient: mockAPI, requestBuilder: mockRequestBuilder)
         delegate = MockWeatherSDKDelegate()
+    }
+    
+    @Test
+    func testLoadCurrentWeather() async {
+        repository.currentWeatherResponse = MockWeatherData.getData()
         service = WeatherService(weatherSDKDelegate: delegate, repository: repository)
+
+        let result = try? await service?.loadCurrentWeather()
+
+        #expect(result?.cityName == "The Weather in Berlin is:")
+        #expect(result?.fomattedTime == "AT 18:00 LOCAL TIME")
+        #expect(result?.formatedTemp == "23°C")
+        #expect(result?.skyCondition == "Clouds")
     }
     
     deinit {
