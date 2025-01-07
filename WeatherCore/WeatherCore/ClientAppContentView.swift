4608ef42ffc3e7f16c59920eab8cd8ca1db94844
qazi <qazi.naveed@nextgeni.com>
2025-01-07 15:53:24 +0100

    update source app view

--- WeatherCore/WeatherCore/ClientAppContentView.swift
+++ WeatherCore/WeatherCore/ClientAppContentView.swift
@@ -9,15 +9,25 @@ import SwiftUI
 
 struct ClientAppContentView: View {
     @ObservedObject var viewModel: MainViewModel
-
+    @State private var isWeatherViewPresented = false // Track the modal presentation state
     var body: some View {
-        VStack {
-            Text("Hello, world!")
-            viewModel.getWeatherSDKView()
-            
-        }
-        .padding()
-    }
+           NavigationStack {
+               Button(action: {
+                   // Trigger SDK-related logic here, if needed.
+                   // Then, present WeatherView modally.
+                   isWeatherViewPresented = true
+               }, label: {
+                   Text("Launch SDK")
+               })
+               .padding()
+               
+               // Present the weather view as a full-screen modal
+               .fullScreenCover(isPresented: $isWeatherViewPresented, content: {
+                   viewModel.getWeatherSDKView()
+                       .edgesIgnoringSafeArea(.all) // Make sure the modal occupies the full screen
+               })
+           }
+       }
 }
 
 #Preview {
