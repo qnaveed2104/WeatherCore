b4cb9167700b9043f2ea32b38b7237939d38768f
qazi <qazi.naveed@nextgeni.com>
2025-01-07 09:34:46 +0100

    Adding file that manages Themes 1- AppColors 2- ThemeFonts 3- WeatherViewWithTheme

---
+++ WeatherCore/WeatherCore/Source/Core/Shared/Theme/Color/AppColors.swift
@@ -0,0 +1,34 @@
+//
+//  AppColors.swift
+//  WeatherCore
+//
+//  Created by Qazi on 07/01/2025.
+//
+ 
+import SwiftUI
+
+enum AppColors {
+    case accent
+        case accentHighlighted
+        case textPrimary
+        case textSecondary
+        case textPlaceholder
+        case textBorder
+        
+        var color: Color {
+            switch self {
+            case .accent:
+                return Color("Accent")
+            case .accentHighlighted:
+                return Color("AccentHighlighted")
+            case .textPrimary:
+                return Color("TextPrimary")
+            case .textSecondary:
+                return Color("TextSecondary")
+            case .textPlaceholder:
+                return Color("TextPlaceholder")
+            case .textBorder:
+                return Color("TextBorder")
+            }
+        }
+}
